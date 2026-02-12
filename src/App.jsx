ba04292a0626a512450805a2996da169b7c55c97
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-02-12 17:55:25 +0500

    initail commit: React-Crypto-Tracker setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CurrencyTracker from './CurrencyTracker'
+const App = () => {
+  return (
+    <>
+    <CurrencyTracker />
+    </>
+  )
+}
+
+export default App
---
+++ src/CurrencyTracker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CurrencyTracker = () => {
+  return (
+    <div>CurrencyTracker</div>
+  )
+}
+
+export default CurrencyTracker
